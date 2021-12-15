b874612d860320d287051b9d773a9760633d1542
itishardtomakeaname <53591243+itishardtomakeaname@users.noreply.github.com>
2021-12-15 10:03:19 +0800

    Create run.py

---
+++ week2/run.py
@@ -0,0 +1,45 @@
+import dgl.function as fn
+import torch as th
+import torch.nn as nn
+import torch.nn.functional as F
+from model import *
+from utils import *
+import time
+import numpy as np
+
+net = Net()
+print(net)
+
+
+
+g, features, labels, train_mask, test_mask = load_cora_data()
+# Add edges between each node and itself to preserve old node representations
+g.add_edges(g.nodes(), g.nodes())
+optimizer = th.optim.Adam(net.parameters(), lr=1e-2)
+dur = []
+best_acc = 0
+best_index = 0
+for epoch in range(50):
+    if epoch >= 3:
+        t0 = time.time()
+
+    net.train()
+    logits = net(g, features)
+    logp = F.log_softmax(logits, 1)
+    loss = F.nll_loss(logp[train_mask], labels[train_mask])
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if epoch >= 3:
+        dur.append(time.time() - t0)
+
+    acc = evaluate(net, g, features, labels, test_mask)
+    if acc > best_acc:
+        best_acc = acc
+        best_index = epoch
+    print("Epoch {:05d} | Loss {:.4f} | Test Acc {:.4f} | Time(s) {:.4f}".format(
+        epoch, loss.item(), acc, np.mean(dur)))
+print(f'{best_index}th epoch best: {best_acc}')
+
